4b165b33a0cbc4bffed8d3bf818b0c82ec72a4d3
ebussiere <eddy@embtech.ca>
2020-10-16 01:15:27 +0000

    LighthouseBnB Users

---
+++ old/server/db-connection.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+const connection = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+connection.connect(() => {
+  console.log('Connected to lightbnb');
+});
+
+module.exports = connection;
